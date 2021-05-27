3a3c362294a010d4a6a98cc53bc0580765d01380
bpotter-trinity <bpotter@trinity.edu>
2021-05-26 19:32:01 -0500

    working on nav bar

--- personal-website2/src/App.js
+++ personal-website2/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss';
+import NavBar from './Components/NavBar.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sidebar">
+        <NavBar />
+      </div>
+
+      <div className="main-content"></div>   
+
+
     </div>
-  );
+  )
 }
 
 export default App;
+
---
+++ personal-website2/src/Components/NavBar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import avatar from '../img/avatar.png';
+import {NavLink} from 'react-router-dom';
+
+function NavBar() {
+    return (
+        <div className="NavBar">
+                
+            <nav className="nav">
+
+                <div className="profile">
+                    <img src={avatar} alt="" /> 
+                </div>
+
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        <NavLink to="/" exact activeClassName="active">
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/about" exact activeClassName="active">
+                            About
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/portfolios" exact activeClassName="active">
+                            Portfolios
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/blogs" exact activeClassName="active">
+                            Blogs
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to="/contacts" exact activeClassName="active">
+                            Contacts
+                        </NavLink>
+                    </li>
+                </ul>
+
+                <footer className="footer">
+                    <p>
+                        @2021 Brendan Potter
+                    </p>
+            </footer>
+            </nav>
+            
+        </div>
+    )
+}
+
+export default NavBar
