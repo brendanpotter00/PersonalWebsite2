516e2c1868e0088b24215098f79a92243120922b
bpotter-trinity <bpotter@trinity.edu>
2021-05-28 01:38:12 -0500

    more on about page

--- personal-website2/src/Components/ImageSection.js
+++ personal-website2/src/Components/ImageSection.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import about from '../img/archPic1ABOUT.png';
 
 function ImageSection() {
     return (
-        <div>
+        <div className="ImageSection">
+            <div className="img">
+                <img src={about} alt ="" />
+            </div>
             
+            <div className="about-info">
+                <h4> I am <span> Brendan Potter</span></h4>
+                <p>
+                    I am gay. this a descriptor and is IN IMAGESECTION.JS
+                </p>
+                <div className="about-details">
+                    <div className="name-details">
+                        <p>Full Name: Brendan N Potter</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
