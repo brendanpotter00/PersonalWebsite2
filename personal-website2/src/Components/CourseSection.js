904391576b1b17de6956380279e5c849d663be74
bpotter-trinity <bpotter@trinity.edu>
2021-06-01 13:01:31 -0500

    details on courses

--- personal-website2/src/Components/CourseSection.js
+++ personal-website2/src/Components/CourseSection.js
@@ -7,7 +7,7 @@ function CourseSection() {
             <div className="cs-title">
                 <h4> Computer <span> Science</span></h4>
                 <p className="cs-description">
-                   All Computer Science courses I have taken at Trinity University as of Fall 2021. IN COURSESECTION.JS 
+                   All computer science courses I have taken at Trinity University as of Fall 2021. IN COURSESECTION.JS 
                 </p>
                 <div className="course-area">
                     <div className="courses">
@@ -17,20 +17,40 @@ function CourseSection() {
                         <p>Low-Level Computing</p>
                         <p>Functional Programming</p>
                         <p>Principals of Data Abstraction</p>
-                        <p>Computer Design</p>
-                        <p>Game AI</p>
-                        <p></p>
+                        <p>Principals of Computer Design</p>
+                        <p>Topics in Game AI</p>
+                        <p>Database Systems</p>
+                        <p>Principals of Theoretical Computer Science</p>
+                        <p>Principals of Algorithms</p>
                         
                     </div>
                     <div className="course-specifications">
-                        <p>: Scala</p>
-                        <p>: Scala</p>
-                        <p>: probability and proofs and logic</p>
-                        <p>: C </p>
-                        <p>: Haskell</p>
-                        <p>: C++</p>
-                        <p>: MIPS</p>
+                        <p>: object-oriented programming in <span className="langs">Scala </span>and basics of computer science</p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p>: object-oriented programming in <span className="langs">Scala </span></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p>: probability, mathmatical proofs, and logic</p>
+                        <p>: learned to program in <span className="langs">C </span></p>
+                        <p></p>
+                        <p>: learned the fundamentals of functinal programming with <span className="langs">Haskell</span></p>
+                        <p></p>
+                        <p></p>
+                        <p></p>
+                        <p>: learned to reverse a linked list, delete a binary tree, etc in <span className="langs">C++</span></p>
+                        <p></p>
+                        <p></p>
+                        <p>: computer design emphasizing logical design, such as computer arithmetic, processor control, etc. And utilized assembly-language programming <span className="langs">(Mips)</span> </p>
                         <p>: Java</p>
+                        <p>: Java</p>
+                        <p>: Java</p>
+                        <p>: Java</p>
+                        <p>: Java</p>
+                        <p>: Java</p>
+                        
                        
                     </div>
                 </div>
@@ -39,23 +59,20 @@ function CourseSection() {
             <div className="cs-title">
                 <h4> General <span> Business</span></h4>
                 <p className="cs-description">
-                   Business is for noobs. and is IN COURSESECTION.JS 
+                    All business courses I have taken at Trinity University as of Fall 2021. IN COURSESECTION.JS 
                 </p>
                 <div className="course-area">
                     <div className="courses">
-                        <p>Stats</p>
-                        <p>Marketing</p>
-                        <p>Managment</p>
-                        <p>Low-Level Computing</p>
+                        <p>Principals of Marketing</p>
+                        <p>Principals of Microeconomics</p>
+                        <p>Managment of Organizations</p>
+                        <p>Fundamentals of Info Systems</p>
                         <p>Functional Programming</p>
                         
                     </div>
                     <div className="course-specifications">
-                        <p>: idek bro</p>
-                        <p>: idek bro</p>
-                        <p>: idek bro</p>
-                        <p>: idek bro</p>
-                        <p>: idek bro</p>
+                        
+                        
                        
                     </div>
                 </div>
