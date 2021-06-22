bc03d5740ceb8293704022dc1b8664c11023309f
bpotter-trinity <bpotter@trinity.edu>
2021-06-22 02:25:04 -0500

    fix

--- personal-website2/src/App.js
+++ personal-website2/src/App.js
@@ -17,6 +17,7 @@ function App() {
   }
   return (
     <div className="App">
+      <HomePage />
       <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
         <NavBar />
       </div>
